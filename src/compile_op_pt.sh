b6cc991c9bad24b25c2200edec4db8521853b8a5
TomerStolik902 <tomerstolik@mail.tau.ac.il.com>
2022-12-10 09:23:05 +0200

    SAGA: Spectral Adversarial Geometric Attack on 3D Meshes

--- src/compile_op_pt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# update paths
-export CUDA_HOME=/usr/local/cuda-10.2
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.2/lib64:/usr/local/cuda-10.2/extras/CUPTI/lib64
-export PATH=$PATH:$CUDA_HOME/bin
-
-# compile 3D-Chamfer Distance op
-python ChamferDistancePytorch/chamfer3D/setup.py install
